81460a515af85e062902abfa3ada9749e612e33e
jose2806 <jose12.jr610@gmail.com>
2024-12-19 09:42:02 -0600

    add firts files

---
+++ src/test/java/com/foro_hub/Foro_Hub/ForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foro_hub.Foro_Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
