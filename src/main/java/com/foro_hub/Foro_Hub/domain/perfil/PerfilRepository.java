42d4103c89490757cfb66f13bf2bbcc28f925642
jose2806 <jose12.jr610@gmail.com>
2025-01-15 10:55:04 -0600

    add PerfilRepository

---
+++ src/main/java/com/foro_hub/Foro_Hub/domain/perfil/PerfilRepository.java
@@ -0,0 +1,6 @@
+package com.foro_hub.Foro_Hub.domain.perfil;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PerfilRepository extends JpaRepository<Perfil,Long> {
+}
