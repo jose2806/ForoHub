2a4eb73e045a512d5e3ed7ac671bc4beef03b892
jose2806 <jose12.jr610@gmail.com>
2025-01-20 16:58:02 -0600

    update CursoController

--- src/main/java/com/foro_hub/Foro_Hub/controller/CursoController.java
+++ src/main/java/com/foro_hub/Foro_Hub/controller/CursoController.java
@@ -1,6 +1,8 @@
 package com.foro_hub.Foro_Hub.controller;
 
 import com.foro_hub.Foro_Hub.domain.curso.*;
+import com.foro_hub.Foro_Hub.domain.topico.Topico;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -31,5 +33,13 @@ public class CursoController {
     @GetMapping
     public ResponseEntity<Page<DatosListadoCurso>> listarCursos(@PageableDefault(size = 3)Pageable paginacion){
         return ResponseEntity.ok(cursoRepository.findAll(paginacion).map(DatosListadoCurso::new));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity eliminarCurso(@PathVariable Long id){
+        Curso curso = cursoRepository.getReferenceById(id);
+        cursoRepository.delete(curso);
+        return ResponseEntity.noContent().build();
     }
 }
